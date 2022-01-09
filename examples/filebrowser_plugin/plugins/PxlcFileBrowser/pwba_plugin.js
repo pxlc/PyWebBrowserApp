26f7c1213c38d7423501658084991d85aec0f63f
pxlc <29380825+pxlc@users.noreply.github.com>
2022-01-09 09:34:52 -0500

    further work on filebrowser plugin ... still wip

--- examples/filebrowser_plugin/plugins/PxlcFileBrowser/pwba_plugin.js
+++ examples/filebrowser_plugin/plugins/PxlcFileBrowser/pwba_plugin.js
@@ -14,7 +14,7 @@ function ${P}() {
             _self.outer_div = document.getElementById("id_${P}_outer");
         }
         _self.outer_div.style.display = "block";
-    }
+    };
 
     _self.hide = function()
     {
@@ -22,7 +22,49 @@ function ${P}() {
             _self.outer_div = document.getElementById("id_${P}_outer");
         }
         _self.outer_div.style.display = "none";
-    }
+    };
+
+    _self.validate_dirpath = function(dirpath_to_validate, callback_fn_name)
+    {
+        console.log(':: inside pwba.${P}.validate_dirpath() function!');
+
+        _self.plugin_to_python("validate_dirpath", {"dirpath_to_validate": dirpath_to_validate,
+                                                    "callback_fn_name": callback_fn_name});
+    };
+    
+    _self.response_to_validate_dirpath = function(op_data)
+    {
+        let is_path_valid = op_data.is_path_valid;
+        let validation_callback_fn_name = op_data.callback_fn_name;
+
+        if (validation_callback_fn_name) {
+            let exec_str = validation_callback_fn_name + '(' + is_path_valid + ');';
+            eval(exec_str);
+        }
+    };
+
+    _self.get_path_value = function()
+    {
+        let path_value = document.getElementById("id_${P}_pathEdit").value;
+        return path_value;
+    };
+
+    _self.set_path_value = function(new_path_value)
+    {
+        let path_text_input_el = document.getElementById("id_${P}_pathEdit");
+        path_text_input_el.value = new_path_value;
+    };
+
+    _self.apply_path_input_validation = function(is_path_valid)
+    {
+        let path_text_input_el = document.getElementById("id_${P}_pathEdit");
+        path_text_input_el.classList.remove("${P}_error", "${P}_success");
+        if (is_path_valid) {
+            path_text_input_el.classList.add("${P}_success");
+        } else {
+            path_text_input_el.classList.add("${P}_error");
+        }
+    };
 }
 
 //
@@ -34,6 +76,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById("id_${P}_pathEdit").onkeyup = function(e) {
         if (e.key === 'Enter' || e.code === 13) {
             console.log(':: Got ENTER key!'); // TODO: validate path entered here
+            pwba.${P}.validate_dirpath(pwba.${P}.get_path_value(), "pwba.${P}.apply_path_input_validation");
         }
     };
 });
--- examples/filebrowser_plugin/plugins/PxlcFileBrowser/pwba_plugin.py
+++ examples/filebrowser_plugin/plugins/PxlcFileBrowser/pwba_plugin.py
@@ -1,4 +1,6 @@
 
+import os
+
 from PyWebBrowserApp import PluginBase
 from PyWebBrowserApp import register_plugin_op
 
@@ -9,6 +11,26 @@ class Plugin(PluginBase):
 
         super(Plugin, self).__init__()
         self.name = '${P}'
+
+    @register_plugin_op
+    def validate_dirpath(self, op_data):
+
+        callback_fn_name = op_data.get('callback_fn_name', '')
+        dirpath_to_validate = op_data.get('dirpath_to_validate', '')
+        is_path_valid = False
+
+        if os.path.isdir(dirpath_to_validate):
+            is_path_valid = True
+        else:
+            # Sym-links manifest as files in Python so see if we can listdir() on it
+            try:
+                os.listdir(dirpath_to_validate)
+                is_path_valid = True
+            except:
+                pass # leave as not valid
+
+        self.plugin_to_webbrowser('response_to_validate_dirpath',
+                                  {'is_path_valid': is_path_valid, 'callback_fn_name': callback_fn_name})
 
     @register_plugin_op
     def test_plugin_callback(self, op_data):
