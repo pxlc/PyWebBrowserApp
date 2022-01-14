e856f0c55f589ee209d7d234caec8dd73211a600
pxlc <29380825+pxlc@users.noreply.github.com>
2022-01-14 14:23:46 -0500

    added special handling to filter out some items that show up when os.listdir() is run on the user USERPROFILE path folder, where some items are not really there on the file system (e.g. "My Documents")

--- plugins/FileBrowser/pwba_plugin.py
+++ plugins/FileBrowser/pwba_plugin.py
@@ -1,5 +1,7 @@
 
 import os
+import sys
+import traceback
 
 from PyWebBrowserApp import PluginBase
 from PyWebBrowserApp import register_plugin_op
@@ -11,40 +13,57 @@ class Plugin(PluginBase):
 
         super(Plugin, self).__init__()
         self.name = '${P}'
+        self.user_profile_path = os.getenv('USERPROFILE').replace('\\', '/') \
+                                    if sys.platform == 'win32' else ''
 
     @register_plugin_op
     def validate_dirpath(self, op_data):
 
-        callback_fn_name = op_data.get('callback_fn_name', '')
-        dirpath_to_validate = op_data.get('dirpath_to_validate', '').replace('\\', '/')
+        try:
+            callback_fn_name = op_data.get('callback_fn_name', '')
+            dirpath_to_validate = os.path.expandvars(op_data.get('dirpath_to_validate', '')).replace('\\', '/')
 
-        if not dirpath_to_validate:
+            if not dirpath_to_validate:
+                self.plugin_to_webbrowser('response_to_validate_dirpath',
+                                        {'is_path_valid': False, 'callback_fn_name': callback_fn_name,
+                                            'dirpath_value': '', 'dir_items_info': dir_items_info})
+            is_path_valid = False
+            dir_items_info = None
+
+            if len(dirpath_to_validate) == 2 and dirpath_to_validate[1] == ':':
+                dirpath_to_validate += '/'
+
+            if os.path.isdir(dirpath_to_validate):
+                is_path_valid = True
+                dir_items_info = {'files': [], 'folders': []}
+
+                do_not_include_list = []
+                if sys.platform == 'win32' and \
+                        dirpath_to_validate.lower() == self.user_profile_path.lower():
+                    do_not_include_list = ['My Documents', 'Application Data', 'Cookies', 'Local Settings',
+                                            'NetHood', 'PrintHood', 'Recent', 'SendTo', 'Start Menu',
+                                            'Templates']
+
+                items = [f for f in sorted(os.listdir(dirpath_to_validate)) if f not in do_not_include_list]
+
+                for i in items:
+                    i_path = '%s/%s' % (dirpath_to_validate, i)
+                    if os.path.isdir(i_path):
+                        dir_items_info['folders'].append(i)
+                    else:
+                        # otherwise assume it is a file
+                        dir_items_info['files'].append(i)
+
             self.plugin_to_webbrowser('response_to_validate_dirpath',
-                                    {'is_path_valid': False, 'callback_fn_name': callback_fn_name,
-                                        'dirpath_value': '', 'dir_items_info': dir_items_info})
-        is_path_valid = False
-        dir_items_info = None
-
-        if len(dirpath_to_validate) == 2 and dirpath_to_validate[1] == ':':
-            dirpath_to_validate += '/'
-        dirpath_to_validate = os.path.expandvars(dirpath_to_validate).replace('\\', '/')
-
-        if os.path.isdir(dirpath_to_validate):
-            is_path_valid = True
-            dir_items_info = {'files': [], 'folders': []}
-
-            items = sorted(os.listdir(dirpath_to_validate))
-            for i in items:
-                i_path = '%s/%s' % (dirpath_to_validate, i)
-                if os.path.isdir(i_path):
-                    dir_items_info['folders'].append(i)
-                else:
-                    # otherwise assume it is a file
-                    dir_items_info['files'].append(i)
-
-        self.plugin_to_webbrowser('response_to_validate_dirpath',
-                                  {'is_path_valid': is_path_valid, 'callback_fn_name': callback_fn_name,
-                                    'dirpath_value': dirpath_to_validate, 'dir_items_info': dir_items_info})
+                                      {'is_path_valid': is_path_valid, 'callback_fn_name': callback_fn_name,
+                                       'dirpath_value': dirpath_to_validate, 'dir_items_info': dir_items_info})
+        except:
+            err_msg = '[in plugin ${P}.validate_dirpath() method] Exception raised ...\n%s' % traceback.format_exc()
+            self.error(err_msg)
+            self.plugin_to_webbrowser('response_to_validate_dirpath',
+                                      {'is_path_valid': 'error', 'callback_fn_name': callback_fn_name,
+                                       'dirpath_value': dirpath_to_validate, 'dir_items_info': None,
+                                       'error_message': err_msg})
 
     @register_plugin_op
     def test_plugin_callback(self, op_data):
